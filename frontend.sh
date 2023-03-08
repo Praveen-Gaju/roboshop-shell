02316a5d31ac1778a776a18275735a0a12fac9bc
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-03-08 18:46:49 +0530

    updating roboshop-shell directory

---
+++ frontend.sh
@@ -0,0 +1,16 @@
+#Install Nginx
+yum install nginx
+
+#Remove the default content that web server is serving.
+rm -rf /usr/share/nginx/html/*
+
+#Download the frontend content
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+#Extract the frontend content.
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+#Start & Enable Nginx service
+systemctl enable nginx
+systemctl start nginx
