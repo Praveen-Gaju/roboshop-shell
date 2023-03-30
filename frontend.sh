566d245555c70ddc0de4abbbe7a4e9ab71b5293c
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-03-30 14:43:37 +0530

    updating roboshop-shell directory

--- frontend.sh
+++ frontend.sh
@@ -1,37 +1,32 @@
+
+
 source common.sh
 
-#Install Nginx
-print_head "Installing Nginx"
+print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
 status_check $?
 
-#Remove the default content that web server is serving.
-print_head "Removing default content in Web Server"
+print_head "Removing Old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 status_check $?
 
-#Download the frontend content
-print_head "Downloading Frontend content"
+print_head "Downloading Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 status_check $?
 
-#Extract the frontend content.
-print_head "Extracting and unzip the frontend content"
+print_head "Extracting Downloaded Frontend"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 status_check $?
 
-#copy nginx configuration file
-print_head "Copy Nginx configuration file"
+print_head "Copying Nginx Config for RoboShop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 status_check $?
 
-#Start & Enable Nginx service
-print_head "Enabaling Nginx"
+print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
 status_check $?
 
-print_head "Restarting Nginx"
+print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}
 status_check $?
-
