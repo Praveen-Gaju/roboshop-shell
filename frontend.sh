6d53ab0a6d495bfd1e84fcb519f8121d2beed10a
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-03-31 12:08:38 +0530

    updating roboshop-shell directory

--- frontend.sh
+++ frontend.sh
@@ -23,14 +23,14 @@ status_check $?
 
 #copy nginx configuration file
 print_head "Copy Nginx configuration file"
-cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+cp "${code_dir}"/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 status_check $?
 
 #Start & Enable Nginx service
-print_head "Enabaling Nginx"
+print_head "Enabling Nginx"
 systemctl enable nginx &>>${log_file}
 status_check $?
 
-print_head "Restarting Nginx"
+print_head "starting Nginx"
 systemctl restart nginx &>>${log_file}
 status_check $?
