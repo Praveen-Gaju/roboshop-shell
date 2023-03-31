32561b5efca79e010e890d2bb47303fa2f67b64f
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-03-31 12:35:35 +0530

    updating roboshop-shell directory

--- frontend.sh
+++ frontend.sh
@@ -10,18 +10,18 @@ print_head "Removing default content in Web Server"
 rm -rf /usr/share/nginx/html/*  &>>${log_file}
 status_check $?
 
-#Download the frontend content
+#Download the frontend content.
 print_head "Downloading Frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 status_check $?
 
 #Extract the frontend content.
 print_head "Extracting and unzip the frontend content"
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>${log_file}
 unzip /tmp/frontend.zip &>>${log_file}
 status_check $?
 
-#copy nginx configuration file
+#copy nginx configuration file.
 print_head "Copy Nginx configuration file"
 cp "${code_dir}"/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 status_check $?
