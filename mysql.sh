3a2ce8f5259b4713b049d1330babd95c4bda208c
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-04-15 18:52:42 +0530

    updating roboshop-shell directory

--- mysql.sh
+++ mysql.sh
@@ -10,26 +10,26 @@ fi
 #So lets disable MySQL 8 version.
 print_head "Disable mysql version 8"
 dnf module disable mysql -y &>>${log_file}
-status check $?
+status_check $?
 
 #Copy repo file to server
 print_head "copy mysql repo file"
 cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo &>>${log_file}
-status check $?
+status_check $?
 
 #Install Mysql server
 print_head "Installing mysql server"
 yum install mysql-community-server -y &>>${log_file}
-status check $?
+status_check $?
 
 #Start MySQL Service
 print_head "Enabling mysql service"
 systemctl enable mysqld &>>${log_file}
-status check $?
+status_check $?
 
 print_head "Starting mysql service"
 systemctl start mysqld &>>${log_file}
-status check $?
+status_check $?
 
 #Next, We need to change the default root password in order to start using the database service.
 #Use password RoboShop@1 or any other as per your choice.
@@ -39,7 +39,7 @@ echo show databases | mysqql -uroot -p${mysql_root_password} &>>${log_file}
 if [ $? -ne 0 ]; then
   mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
 fi
-status check $?
+status_check $?
 
 #You can check the new password working or not using the following command in MySQL.
 # mysql -uroot -pRoboShop@1
