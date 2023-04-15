98acece62038cb0256b910d18cb9b1784624d2c3
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-04-15 19:15:05 +0530

    updating roboshop-shell directory

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -9,31 +9,31 @@ fi
 #Configure YUM Repos from the script provided by vendor.
 print_head "Setting up Erlang Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>{log_file}
-status check $?
+status_check $?
 
 #Configure YUM Repos for RabbitMQ.
 print_head "Setting up rabbitmq repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>{log_file}
-status check $?
+status_check $?
 
 #Install Erlang
 print_head "Installing Erlang and Rabbitmq"
 yum install erlang -y &>>{log_file}
-status check $?
+status_check $?
 
 #Install RabbitMQ
 print_head "Installing Rabbitmq service"
 yum install rabbitmq-server -y &>>{log_file}
-status check $?
+status_check $?
 
 #Start RabbitMQ Service
 print_head "Enabling Rabbitmq service"
 systemctl enable rabbitmq-server &>>{log_file}
-status check $?
+status_check $?
 
 print_head "Starting Rabbitmq service"
 systemctl start rabbitmq-server &>>{log_file}
-status check $?
+status_check $?
 
 #RabbitMQ comes with a default username / password as guest/guest. But this user cannot be used to connect.
 #Hence, we need to create one user for the application.
@@ -42,9 +42,9 @@ rabbitmqctl list_users | grep roboshop &>>{log_file}
 if [ $? -ne 0 ]; then
   rabbitmqctl add_user roboshop ${roboshop_app_password} &>>{log_file}
 fi
-status check $?
+status_check $?
 
 #rabbitmqctl set_user_tags roboshop administrator
 print_head "Configure permissions for app user"
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>{log_file}
-status check $?
+status_check $?
